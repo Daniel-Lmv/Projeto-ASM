3ab3953e5f129185a2b0b2e700d4b049f083fb6d
Maiiron <mayronlimaverde@gmail.com>
2026-07-20 17:31:39 -0400

    add: backend/alembic
    add: backend/alembic

---
+++ backend/app/database/models/__init__.py
@@ -0,0 +1,5 @@
+from .user_model import UserModel
+from .category_model import CategoryModel
+from .budget_cycle_model import BudgetCycleModel
+from .income_model import IncomeModel
+from .expense_model import ExpenseModel
